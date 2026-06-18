ac078780aa33dc044d4df64c6e096870c19d7900
theophilus-123-del <theophilusaidoo42@gmail.com>
2026-06-18 10:18:01 -0700

    Add files via upload

---
+++ q9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable declaration
+    int x;
+
+    // Assignment operator
+    x = 10;
+    cout << "Assignment Operator (=): x = " << x << endl;
+
+    // Equality operator
+    if (x == 10)
+    {
+        cout << "Equality Operator (==): x is equal to 10" << endl;
+    }
+
+    return 0;
+}
